3572f361640988d81f385575f1b4d525d6beb6d3
Mohannad Sabea <Moh4nn4d@gmail.com>
2023-03-07 16:16:26 +0200

    Products Section relates #10

--- home/home.js
+++ home/home.js
@@ -5,15 +5,17 @@ const products = [
     detail: "Lorem ipsum dolor sit amet consectetur",
     cat: "smart",
     price: 50,
-    imageUrl: "https://unsplash.com/photos/oXXc-s5nNy8",
+    imageUrl:
+      "https://images.unsplash.com/photo-1585298723682-7115561c51b7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80",
   },
 
   {
-    name: "sony camera",
+    name: "Sony Camera",
     detail: "Lorem ipsum dolor sit amet consectetur",
     cat: "smart",
     price: 750,
-    imageUrl: "https://unsplash.com/photos/6qesnUQceJA",
+    imageUrl:
+      "https://images.unsplash.com/photo-1606986628470-26a67fa4730c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
   },
 
   {
@@ -21,7 +23,8 @@ const products = [
     detail: "Lorem ipsum dolor sit amet consectetur",
     cat: "smart",
     price: 500,
-    imageUrl: "https://unsplash.com/photos/ZwKCWVFdrcs",
+    imageUrl:
+      "https://images.unsplash.com/photo-1589492477829-5e65395b66cc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
   },
 
   {
@@ -29,7 +32,8 @@ const products = [
     detail: "Lorem ipsum dolor sit amet consectetur",
     cat: "smart",
     price: 1000,
-    imageUrl: "https://unsplash.com/photos/mP7aPSUm7aE",
+    imageUrl:
+      "https://images.unsplash.com/photo-1517336714731-489689fd1ca8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1026&q=80",
   },
 
   {
@@ -37,7 +41,8 @@ const products = [
     detail: "Lorem ipsum dolor sit amet consectetur",
     cat: "fashion",
     price: 40,
-    imageUrl: "https://unsplash.com/photos/jvoZ-Aux9aw",
+    imageUrl:
+      "https://images.unsplash.com/photo-1595950653106-6c9ebd614d3a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
   },
 
   {
@@ -45,7 +50,8 @@ const products = [
     detail: "Lorem ipsum dolor sit amet consectetur",
     cat: "fashion",
     price: 40,
-    imageUrl: "https://unsplash.com/photos/7cERndkOyDw",
+    imageUrl:
+      "https://images.unsplash.com/photo-1620799140408-edc6dcb6d633?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1072&q=80",
   },
 
   {
@@ -55,6 +61,69 @@ const products = [
     price: 70,
     imageUrl:
       "https://www.istockphoto.com/photo/beautiful-denim-pants-gm639511940-115301581?phrase=trousers",
+  },
+
+  {
+    name: "Closed-Back Headphones",
+    detail: "Lorem ipsum dolor sit amet consectetur",
+    cat: "smart",
+    price: 50,
+    imageUrl:
+      "https://images.unsplash.com/photo-1585298723682-7115561c51b7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80",
+  },
+
+  {
+    name: "sony camera",
+    detail: "Lorem ipsum dolor sit amet consectetur",
+    cat: "smart",
+    price: 750,
+    imageUrl:
+      "https://images.unsplash.com/photo-1606986628470-26a67fa4730c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  },
+
+  {
+    name: "iphone 12",
+    detail: "Lorem ipsum dolor sit amet consectetur",
+    cat: "smart",
+    price: 500,
+    imageUrl:
+      "https://images.unsplash.com/photo-1589492477829-5e65395b66cc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+  },
+
+  {
+    name: "laptop",
+    detail: "Lorem ipsum dolor sit amet consectetur",
+    cat: "smart",
+    price: 1000,
+    imageUrl:
+      "https://images.unsplash.com/photo-1517336714731-489689fd1ca8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1026&q=80",
+  },
+
+  {
+    name: "shoes",
+    detail: "Lorem ipsum dolor sit amet consectetur",
+    cat: "fashion",
+    price: 40,
+    imageUrl:
+      "https://images.unsplash.com/photo-1595950653106-6c9ebd614d3a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+  },
+
+  {
+    name: "shirt",
+    detail: "Lorem ipsum dolor sit amet consectetur",
+    cat: "fashion",
+    price: 30,
+    imageUrl:
+      "https://images.unsplash.com/photo-1620799140408-edc6dcb6d633?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1072&q=80",
+  },
+
+  {
+    name: "trouser",
+    detail: "Lorem ipsum dolor sit amet consectetur",
+    cat: "fashion",
+    price: 70,
+    imageUrl:
+      "https://images.unsplash.com/photo-1620799140408-edc6dcb6d633?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1072&q=80",
   },
 ];
 
